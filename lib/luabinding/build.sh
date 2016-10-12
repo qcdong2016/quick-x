fce8d11ac6b99b079b5378b984fa28bd29baea8f
yinjimmy <jimmy.yin5@gmail.com>
2016-10-13 00:10:50 +0800

    update CCSpawn & CCSequence lua binding

--- lib/luabinding/build.sh
+++ lib/luabinding/build.sh
@@ -3,4 +3,4 @@ DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 TOLUA="$QUICK_COCOS2DX_ROOT/bin/mac/tolua++"
 
 cd "$DIR/"
-${TOLUA} -L "$DIR/basic.lua" -o "$QUICK_COCOS2DX_ROOT/lib/cocos2d-x/scripting/lua/cocos2dx_support/LuaCocos2d.cpp" Cocos2d.tolua
+${TOLUA} -L "$DIR/basic.lua" -o "$QUICK_COCOS2DX_ROOT/lib/cocos2d-x/scripting/LuaCocos2d.cpp" Cocos2d.tolua
