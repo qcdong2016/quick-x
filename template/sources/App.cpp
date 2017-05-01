8fcf9c2a7b121d98c3dca1846f430dfe917f092f
qcdong <qcdong@yeah.net>
2017-05-01 11:49:51 +0800

    App

---
+++ template/sources/App.cpp
@@ -0,0 +1,67 @@
+#include "CCApplication.h"
+
+#include "audio/SimpleAudioEngine.h"
+#include "scripting/CCLuaEngine.h"
+#include <string>
+#include "CCEGLView.h"
+
+#include "engine/CCModule.h"
+#include "engine/CCDirector.h"
+#include "spine/spine-cocos2dx.h"
+#include "imgui/NoUI.h"
+
+using namespace cocos2d;
+class  AppDelegate : public cocos2d::CCApplication
+{
+public:
+	AppDelegate();
+	virtual ~AppDelegate() {
+		// end simple audio engine here, or it may crashed on win32
+		SimpleAudioEngine::sharedEngine()->end();
+		ModuleManager::shutdown();//re-write
+	}
+
+	bool AppDelegate::applicationDidFinishLaunching()
+	{
+		ModuleManager::addModule<SpineModule>();
+#if OS_IS(MAC) || OS_IS(WIN)
+		ModuleManager::addModule<NoUIModule>();
+#endif
+
+		// initialize director
+		CCDirector *pDirector = CCDirector::sharedDirector();
+		pDirector->setOpenGLView(CCEGLView::sharedOpenGLView());
+		pDirector->setProjection(kCCDirectorProjection2D);
+
+		// set FPS. the default value is 1.0/60 if you don't call this
+		pDirector->setAnimationInterval(1.0 / 60);
+
+		// register lua engine
+		CCLuaEngine *pEngine = CCLuaEngine::defaultEngine();
+		CCScriptEngineManager::sharedManager()->setScriptEngine(pEngine);
+		pEngine->executeScriptFile("scripts/main.lua");
+
+		return true;
+	}
+
+	// This function will be called when the app is inactive. When comes a phone call,it's be invoked too
+	void AppDelegate::applicationDidEnterBackground()
+	{
+		CCDirector::sharedDirector()->stopAnimation();
+		CCDirector::sharedDirector()->pause();
+		SimpleAudioEngine::sharedEngine()->pauseBackgroundMusic();
+		SimpleAudioEngine::sharedEngine()->pauseAllEffects();
+	}
+
+	// this function will be called when the app is active again
+	void AppDelegate::applicationWillEnterForeground()
+	{
+		CCDirector::sharedDirector()->startAnimation();
+		CCDirector::sharedDirector()->resume();
+		SimpleAudioEngine::sharedEngine()->resumeBackgroundMusic();
+		SimpleAudioEngine::sharedEngine()->resumeAllEffects();
+	};
+};
+
+
+QUICKX_APP(AppDelegate)
